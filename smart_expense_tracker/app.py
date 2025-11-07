cd9186b7deb3a59d08d835c644bd70f1ce21ca6f
Angadprajapati7 <154367517+Angadprajapati7@users.noreply.github.com>
2025-11-07 19:04:15 +0530

    Update app.py

--- smart_expense_tracker/app.py
+++ smart_expense_tracker/app.py
@@ -105,5 +105,7 @@ def new_dashboard():
                            category_values=category_values,
                            suggestion=suggestion)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
